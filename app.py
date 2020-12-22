5a249e31332964c1befa07bc69755cddffe924cd
cnulenka <sinulenka001@gmail.com>
2020-12-22 12:32:27 +0530

    add error handling to db session interactions in controller

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
-
+import sys
 
 app = Flask(__name__)
 
@@ -21,13 +21,24 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  description = request.json['description']
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return jsonify({
-    'description': todo.description
-  })
+  error = False
+  body = {}
+  try:
+    description = request.json['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort (400)
+  else:
+    return jsonify(body)
 
 @app.route('/')
 def index():
