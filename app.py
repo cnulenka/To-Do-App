3a2e54f68e57fd1eb3c4351495d4d203db381a75
cnulenka <sinulenka001@gmail.com>
2020-12-23 12:26:53 +0530

    added todolists table

--- app.py
+++ app.py
@@ -15,9 +15,20 @@ class Todo(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
   completed = db.Column(db.Boolean, nullable=False, default=False)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable = False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
+
+class TodoList(db.Model):
+  __tablename__ = 'todolists'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+  todos = db.relationship('Todo', backref ='list', lazy = True)
+
+  def __repr__(self):
+    return f'<TodoList {self.id} {self.name}>'
+
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
